06b87cd9f20358058c64e7be600756190d158398
Ankit Kushawaha <ankitkushawaha4547@gmail.com>
2023-01-12 10:47:27 +0530

    first commit

---
+++ app/src/main/java/com/ankit/button/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ankit.button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+       button= findViewById(R.id.button);
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Log.d("test", "OHH! DONE");
+
+           }
+       });
+
+
+    }
+}
